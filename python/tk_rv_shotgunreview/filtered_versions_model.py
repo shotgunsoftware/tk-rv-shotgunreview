963321dd7769e8fdd5f8ae004da191559079c02d
Stewart Birnam <stewart.birnam@autodesk.com>
2016-04-28 07:06:44 -0700

    adding new model for preset queries

---
+++ python/tk_rv_shotgunreview/filtered_versions_model.py
@@ -0,0 +1,35 @@
+# import the shotgun_model module from the shotgun utils framework
+import tank
+from PySide import QtCore
+
+shotgun_model = tank.platform.import_framework("tk-framework-shotgunutils", "shotgun_model")
+SimpleShotgunModel = shotgun_model.SimpleShotgunModel
+ShotgunModel = shotgun_model.ShotgunModel
+
+
+class FilteredVersionsModel(SimpleShotgunModel):
+
+    def __init__(self, parent, bg_task_manager=None):
+        SimpleShotgunModel.__init__(self, parent)
+        self._RV_DATA_ROLE = QtCore.Qt.UserRole + 1138
+
+
+    # current ShotgunModel inmplentation for reference
+    # def _load_data(
+    #     self, entity_type, filters, hierarchy, fields, order=None, seed=None, limit=None,
+    #     columns=None, additional_filter_presets=None
+    # ):
+
+
+    def load_data(self, entity_type, filters=None, fields=None, hierarchy=None, order=None, additional_filter_presets=None):
+        filters = filters or []
+        fields = fields or ["code"]
+        hierarchy = hierarchy or [fields[0]]
+        ShotgunModel._load_data(self, entity_type, filters, hierarchy, fields, order, None, None, None, additional_filter_presets)
+        self._refresh_data()
+
+    def flags(self, index):
+        if not index.isValid():
+            return QtCore.Qt.NoItemFlags
+        return QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
+
