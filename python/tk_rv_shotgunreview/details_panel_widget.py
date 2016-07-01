8f3f3aaf2310c420207f5bc437b80ebba57fa3b2
Jeff Beeland <jeff.beeland@autodesk.com>
2016-06-30 18:04:24 -0700

    #37270: Sets user thumbs to not show a clickable "hand" cursor.

--- python/tk_rv_shotgunreview/details_panel_widget.py
+++ python/tk_rv_shotgunreview/details_panel_widget.py
@@ -209,6 +209,7 @@ class DetailsPanelWidget(QtGui.QWidget):
         self.ui.note_stream_widget.set_bg_task_manager(self._task_manager)
         self.ui.note_stream_widget.show_sg_stream_button = False
         self.ui.note_stream_widget.version_items_playable = False
+        self.ui.note_stream_widget.clickable_user_icons = False
         self.shot_info_model = shotgun_model.SimpleShotgunModel(
             self.ui.note_stream_widget,
             bg_task_manager=self._task_manager,
