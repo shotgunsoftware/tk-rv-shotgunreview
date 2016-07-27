7da3fd8dfef2282e01b6b0673f8399e642952a13
Stewart Birnam <stewart.birnam@autodesk.com>
2016-07-27 14:50:37 -0700

    cleanup, comments and globalPos()

--- python/tk_rv_shotgunreview/tray_title_bar.py
+++ python/tk_rv_shotgunreview/tray_title_bar.py
@@ -12,37 +12,46 @@ import sys
 from tank.platform.qt import QtCore, QtGui
 
 class TrayTitleBar(QtGui.QFrame):
+    """
+    when the tray is floating, and you click on a menu but dont select anything, that is
+    mouse off the menu, the menu remains open, and you can move the dock but the menu tears
+    off and remains stationary. 
+
+    the menus remain open because the session window is active and receiving their events
+    which are getting eaten by the dock.
+
+    so instead of using the default native titlebar, we make a custom one that we can use
+    to force the dock window to be active, thus auto-closing the menus, for that we have to
+    implement the drag and consume the event.
+    """
 
     def __init__(self, parent):
         QtGui.QFrame.__init__(self, parent)
         self.setStyleSheet("QWidget { background: rgb(100,99,98); margin-bottom: 3px; }")
-        self._last_pos = self.pos()
+        self._clicked_pos = None
+
 
     def mouseMoveEvent(self, event):
-        # self.activateWindow()
-        d = self._last_pos - event.pos()
-        print "D: %r %r" % (d, self.parent().parent())
-        sys.stdout.flush()
+        # we often get here without a starting Press event, since that gets
+        # consumed by the menu's active window. So if clicked pos is none,
+        # we'll just use our current position as the click point.
+        if not self._clicked_pos:
+            self._clicked_pos = event.pos()
 
-        # de-bouncing - sometimes we never get a press event.
-        if d.x() > -50 and d.x() < 50:
-            self.parent().move(self.parent().pos() - (d))
+        self.parent().move(event.globalPos() - self._clicked_pos )  
+        event.accept()
 
-        self._last_pos = event.pos() - d     
-        event.ignore()
 
     def mousePressEvent(self, event):
-        #self._last_pos = event.pos()     
+        self._clicked_pos = event.pos()
         event.ignore()
 
     def mouseReleaseEvent(self, event):
-        #self._last_pos = event.pos()  
+        self._clicked_pos = None
         event.ignore()
 
     def sizeHint(self):
-        self._last_pos = self.pos()
         return QtCore.QSize(1280,14)
 
     def minimumSize(self):
-        self._last_pos = self.pos()
         return QtCore.QSize(980,14)
