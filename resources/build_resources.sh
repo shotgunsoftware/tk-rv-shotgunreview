928753e45d7a704de9236d241e025c92008248d2
Norberto Moreno <norbermv.dev@gmail.com>
2022-03-28 09:58:37 -0500

    SG-25870 tk-rv-shotgunreview Rebranding (#42)
    * ShotGrid branding updates.

--- resources/build_resources.sh
+++ resources/build_resources.sh
@@ -12,9 +12,9 @@
 
 # The path to output all built .py files to: 
 UI_PYTHON_PATH=../python/tk_rv_shotgunreview/ui
-
+PYTHON_BASE="/Applications/Shotgun.app/Contents/Resources/Python"
 # Clear up potential png profile problems.
-for f in *png;do convert -strip $f $f; done
+for f in *png;do mogrify $f; done
 
 # Helper functions to build UI files
 function build_qt {
@@ -24,15 +24,15 @@ function build_qt {
     $1 $2 > $UI_PYTHON_PATH/$3.py
     
     # replace PySide imports with tank.platform.qt and remove line containing Created by date
-    sed -i "" -e "s/from PySide import/from tank.platform.qt import/g" -e "/# Created:/d" $UI_PYTHON_PATH/$3.py
+    sed -i $UI_PYTHON_PATH/$3.py -e "s/from PySide import/from tank.platform.qt import/g" -e "/# Created:/d"
 }
 
 function build_ui {
-    build_qt "pyside-uic --from-imports" "$1.ui" "$1"
+    build_qt "${PYTHON_BASE}/bin/python ${PYTHON_BASE}/bin/pyside-uic --from-imports" "$1.ui" "$1"
 }  
 
 function build_res {
-    build_qt "pyside-rcc" "$1.qrc" "$1_rc"
+    build_qt "${PYTHON_BASE}/bin/pyside-rcc -py3" "$1.qrc" "$1_rc"
 }
 
 
